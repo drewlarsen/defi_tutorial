5004c0e99cfe52d6df595582af389ade128590cb
drewlarsen <drewlarsen@users.noreply.github.com>
2021-11-06 13:40:58 -0600

    add tokenfarm and initial tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,19 @@
+const BearToken = artifacts.require("BearToken");
+const DaiToken = artifacts.require("DaiToken");
 const TokenFarm = artifacts.require("TokenFarm");
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = async function(deployer, network, accounts) {
+
+    await deployer.deploy(DaiToken);
+    const daiToken = await DaiToken.deployed();
+
+    await deployer.deploy(BearToken);
+    const bearToken = await BearToken.deployed();
+
+    await deployer.deploy(TokenFarm, bearToken.address, daiToken.address);
+    const tokenFarm = await TokenFarm.deployed();
+
+    await bearToken.transfer(tokenFarm.address, '1000000000000000000000000');
+
+    await daiToken.transfer(accounts[1], '100000000000000000000');
 };
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,6 +1,17 @@
 pragma solidity ^0.5.0;
 
+import "./BearToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
     string public name = "Bear Token Farm";
+
+    BearToken public bearToken;
+    DaiToken public daiToken;
+
+    constructor(BearToken _bearToken, DaiToken _daiToken) public {
+        bearToken = _bearToken;
+        daiToken = _daiToken;
+    }
 
 }
